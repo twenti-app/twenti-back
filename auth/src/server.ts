5d05bfa07564e4c5e29671b3f1ea50f59bfed00b
rumabe97 <rumabe97@gmail.com>
2022-11-21 12:45:25 +0100

    Config auth

---
+++ auth/src/server.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from "body-parser";
+import {initFirebase} from "./config/InitFirebase";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false, limit: '50mb'}));
+app.use(bodyParser.json({limit: '50mb'}));
+
+initFirebase();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`)
+})
+
+process.on('uncaughtException', err => {
+    console.log('Caught exception ', err);
+})
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.log(`Unhandled rejection at ${promise} reason: ${reason}`)
+})
+
+
